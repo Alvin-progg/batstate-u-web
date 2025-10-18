a36633dc8058f17fdb0629d40aa074b53c2b585b
Alvin-progg <doctoralvin726@gmail.com>
2025-10-18 15:42:38 +0800

    Fix Overlflow of content

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Nav() {
   return (
-    <nav className="fixed top-0 left-0  w-full bg-white shadow-md z-10">
+    <nav className="fixed top-0 left-0  w-full bg-white shadow-md z-30">
       <div className=" gap-4 px-4 py-2 w-fit">
         <Link className="flex items-center gap-4" href="/">
           <Image src="/logo.png" alt="Logo" width={50} height={50} />
