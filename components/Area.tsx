6033a57abb02a0bb7e94bd53ef8086afd8a9d8ca
Alvin-progg <doctoralvin726@gmail.com>
2025-10-19 14:18:11 +0800

    qw2q2n

--- components/Area.tsx
+++ components/Area.tsx
@@ -1,29 +1,49 @@
 import { areaInfo } from "@/data/areaInfo";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Area() {
   return (
-    <section className="flex items-center justify-center w-full">
-      <div className="">
-        <ul className="flex flex-col gap-8 my-8">
+    <section className="flex items-center justify-center w-full overflow-hidden">
+      <div className="flex my-8 gap-2.5">
+        <ul className="flex flex-col gap-8 ">
           {areaInfo.map((area) => (
-            <li key={area.id} className="flex flex-col gap-2">
+            <li key={area.id} className="flex flex-col gap-1">
               <div className="flex items-center gap-4">
-                <span className="text-2xl font-bold text-red-600">
+                <span className="text-3xl font-bold text-red-600">
                   {area.number}
                 </span>
                 <h2 className="text-md text-red-600 font-bold">{area.title}</h2>
               </div>
-              <p className="text-gray-700 text-sm w-[560px] italic">{area.description}</p>
-              <Link 
-                href="#" 
-                className="w-fit text-sm italic text-gray-600 font-semibold"
-              >
-                Read more...
-              </Link>
+              <div className="px-10">
+                <p className="text-gray-700 text-sm w-[560px] italic">
+                  {area.description}
+                </p>
+                <Link
+                  href="#"
+                  className="w-fit text-sm italic text-gray-600 font-semibold"
+                >
+                  Read more...
+                </Link>
+              </div>
             </li>
           ))}
         </ul>
+        <div>
+          <Link
+            href="https://batstateu.edu.ph/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/area-logo.png"
+              alt="Area Image"
+              width={380}
+              height={380}
+              className="ml-12 mt-4"
+            />
+          </Link>
+        </div>
       </div>
     </section>
   );
