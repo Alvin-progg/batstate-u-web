11e6c242d31df2542826a3c3670310505580f0b7
Alvin-progg <doctoralvin726@gmail.com>
2025-10-19 16:38:03 +0800

    12eqw

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Nav />
-        <main className="pt-16">{children}</main>
+        <main className="pt-10">{children}</main>
         <Footer />
       </body>
     </html>
