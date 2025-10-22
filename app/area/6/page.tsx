43896e58cba0218b7549abf11be49fa6aa2f0d2e
francissprog <francissprog@gmail.com>
2025-10-22 10:19:49 +0800

    fasdfa

---
+++ app/area/6/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <section className="flex items-center justify-center w-full overflow-hidden px-4 lg:px-0">
+      <div>Area h</div>
+    </section>
+  );
+};
+
+export default page;
