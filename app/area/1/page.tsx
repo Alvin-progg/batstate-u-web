97e42282e5906b4312eb0858e7ae71d01a5b7667
Alvin-progg <doctoralvin726@gmail.com>
2025-10-23 15:52:38 +0800

    wqa2q2q2

--- app/area/1/page.tsx
+++ app/area/1/page.tsx
@@ -14,7 +14,7 @@ export default async function AreaPage1() {
       parameters={[
         {
           letter: "A",
-          title: "Statement of Vision, Mission, Goals and Objectives",
+          title: "Academic Qualifications and Professional Experience",
           items: [
             { name: "SYSTEM. Inputs and Processes", href: "#" },
             { name: "IMPLEMENTATION", href: "#" },
@@ -23,13 +23,67 @@ export default async function AreaPage1() {
         },
         {
           letter: "B",
-          title: "Dissemination and Acceptability",
+          title: "Recruitment, Selection and Orientation",
           items: [
             { name: "SYSTEM. Inputs and Processes", href: "#" },
             { name: "IMPLEMENTATION", href: "#" },
             { name: "OUTCOMES", href: "#" },
           ],
         },
+                {
+          letter: "C",
+          title: "Faculty Adequacy and Loading",
+          items: [
+            { name: "SYSTEM. Inputs and Processes", href: "#" },
+            { name: "IMPLEMENTATION", href: "#" },
+            { name: "OUTCOMES", href: "#" },
+          ],
+        },
+        {
+          letter: "D",
+          title: "Rank and Tenure",
+          items: [
+            { name: "SYSTEM. Inputs and Processes", href: "#" },
+            { name: "IMPLEMENTATION", href: "#" },
+            { name: "OUTCOMES", href: "#" },
+          ],
+        },
+                {
+          letter: "E",
+          title: "Faculty Development",
+          items: [
+            { name: "SYSTEM. Inputs and Processes", href: "#" },
+            { name: "IMPLEMENTATION", href: "#" },
+            { name: "OUTCOMES", href: "#" },
+          ],
+        },
+        {
+          letter: "F",
+          title: "Professional Performance and Scholarly Works",
+          items: [
+            { name: "SYSTEM. Inputs and Processes", href: "#" },
+            { name: "IMPLEMENTATION", href: "#" },
+            { name: "OUTCOMES", href: "#" },
+          ],
+        },
+                {
+          letter: "G",
+          title: "Salaries, Fringe Benefits and Incentives",
+          items: [
+            { name: "SYSTEM. Inputs and Processes", href: "#" },
+            { name: "IMPLEMENTATION", href: "#" },
+            { name: "OUTCOMES", href: "#" },
+          ],
+        },
+        {
+          letter: "H",
+          title: "Professionalism",
+          items: [
+            { name: "SYSTEM. Inputs and Processes", href: "#" },
+            { name: "IMPLEMENTATION", href: "#" },
+            { name: "OUTCOMES", href: "#" },
+          ],
+        }
       ]}
     />
   );
