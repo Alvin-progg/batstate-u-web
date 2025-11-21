cfdf369e8a3b44f6707a6e0f8b94e96da2d255f4
Joshua Abella <joshuaabella@Joshuas-MacBook-Pro.local>
2025-11-22 00:49:56 +0800

    Adding link to all parameters of Area 9

--- app/area/9/page.tsx
+++ app/area/9/page.tsx
@@ -14,49 +14,41 @@ export default async function AreaPage9() {
       parameters={[
         {
           letter: "A",
-          title: "Parameter A Title",
+          title: "Laboratories, Shops and Facilities",
           items: [
-            { name: "SYSTEM. Inputs and Processes", href: "#" },
-            { name: "IMPLEMENTATION", href: "#" },
-            { name: "OUTCOMES", href: "#" },
+            { name: "SYSTEM. Inputs and Processes", href: "https://drive.google.com/drive/folders/1FpTsSMFcR9FKmcF8yIJZOtUDyimXKb1c?usp=drive_link" },
+            { name: "IMPLEMENTATION", href: "https://drive.google.com/drive/folders/1Q7qYHODPmSl6_41PwNPF-yvonYQ1f8Eg?usp=drive_link" },
+            { name: "OUTCOMES", href: "https://drive.google.com/drive/folders/1JU0v2gQn76-ZzD1xfH2i8_7Ezyz6C4AA?usp=drive_link" },
           ],
         },
         {
           letter: "B",
-          title: "Parameter B Title",
+          title: "Equipment, Supplies and Materials",
           items: [
-            { name: "SYSTEM. Inputs and Processes", href: "#" },
-            { name: "IMPLEMENTATION", href: "#" },
-            { name: "OUTCOMES", href: "#" },
+            { name: "SYSTEM. Inputs and Processes", href: "https://drive.google.com/drive/folders/1FpVpcEWrAcsGuGMZtxkznfMof6im0uS5?usp=drive_link" },
+            { name: "IMPLEMENTATION", href: "https://drive.google.com/drive/folders/11LqCvWwZHsXaJg2sfjVcWsssCEWk5YTC?usp=drive_link" },
+            { name: "OUTCOMES", href: "https://drive.google.com/drive/folders/1ohb6m7sUUR19FL5xSokp0fZ-mUpCTRVA?usp=drive_link" },
           ],
         },
         {
           letter: "C",
-          title: "Parameter C Title",
+          title: "Maintenance",
           items: [
-            { name: "SYSTEM. Inputs and Processes", href: "#" },
-            { name: "IMPLEMENTATION", href: "#" },
-            { name: "OUTCOMES", href: "#" },
+            { name: "SYSTEM. Inputs and Processes", href: "https://drive.google.com/drive/folders/1h2mOcqK4bosNkLXC6SC6Xeijmm4gxj7a?usp=drive_link" },
+            { name: "IMPLEMENTATION", href: "https://drive.google.com/drive/folders/15PTcjzyus2hCcknLbRnwB_dMmCEJbSmt?usp=drive_link" },
+            { name: "OUTCOMES", href: "https://drive.google.com/drive/folders/1_tJD619wtr7Cai5lNDqKBwYGVkiUnKuU?usp=drive_link" },
           ],
         },
         {
           letter: "D",
-          title: "Parameter D Title",
+          title: "Special Provisions",
           items: [
-            { name: "SYSTEM. Inputs and Processes", href: "#" },
-            { name: "IMPLEMENTATION", href: "#" },
-            { name: "OUTCOMES", href: "#" },
+            { name: "SYSTEM. Inputs and Processes", href: "https://drive.google.com/drive/folders/13pjUEIApLuHy1NWc6WWd1A6d_IxOrAPD?usp=drive_link" },
+            { name: "IMPLEMENTATION", href: "https://drive.google.com/drive/folders/1ppM3CWPL8fhYtKwMEWS4-D948upZKAdD?usp=drive_link" },
+            { name: "OUTCOMES", href: "https://drive.google.com/drive/folders/12iZxL5-gTGjWsRtEZTaRrY5zDmVqF6nR?usp=drive_link" },
           ],
         },
-        {
-          letter: "E",
-          title: "Parameter E Title",
-          items: [
-            { name: "SYSTEM. Inputs and Processes", href: "#" },
-            { name: "IMPLEMENTATION", href: "#" },
-            { name: "OUTCOMES", href: "#" },
-          ],
-        },
+       
       ]}
     />
   );
